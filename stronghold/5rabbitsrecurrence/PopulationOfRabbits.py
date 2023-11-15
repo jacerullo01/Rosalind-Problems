9097c1f2ee3951fed9f4688d1c71e0e59b36d000
Jack Cerulo <jcAlpaca@outlook.com>
2023-11-14 17:29:53 -0700

    Working on fibbonacci sequence

--- stronghold/5rabbitsrecurrence/PopulationOfRabbits.py
+++ stronghold/5rabbitsrecurrence/PopulationOfRabbits.py
@@ -1,8 +1,6 @@
-# Original fibbonacci sequence is Fn = Fn-1 + Fn-2 where F1=F2=1
-# Mine is Fn = Fn-1 + K(Fn-2) where F1=F2=1, where k and n are given
-n = 12
-k = 1
-pop = [1,1] # index 0 is n=1, index 1 is n=2 and so on
+n = 33
+k = 5
+pop = [1,1]
 for i in range(2, n):
-    pop.append(pop[i-1] + k*[i-2])
-print(pop)
+    pop.append(pop[i-1] + k * pop[i-2])
+print(pop[n-1])
