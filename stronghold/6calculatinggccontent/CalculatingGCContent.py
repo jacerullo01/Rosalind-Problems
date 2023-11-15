feb70ca21156392815bac144d448bea7931b08d6
Jack Cerulo <jcAlpaca@outlook.com>
2023-11-15 12:15:53 -0700

    working on gc content

--- stronghold/6calculatinggccontent/CalculatingGCContent.py
+++ stronghold/6calculatinggccontent/CalculatingGCContent.py
@@ -7,12 +7,26 @@ CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAG
 '''
 dataset = dataset.split('\n')
 print(dataset)
-DNA1 = dataset[1]
-DNA2 = dataset[3]
-DNA3 = dataset[5]
+def getDNA(dataset):
+    DNAList = []
+    for DNA in range(len(dataset)):
+        if DNA%2 == 1:
+            DNAList+=dataset[DNA]
+
+    return DNAList
+
+print(getDNA(dataset))
+
+DNA1 = list(dataset[1])
+DNA2 = list(dataset[3])
+DNA3 = list(dataset[5])
+def getCG(DNA):
+    CG = 0
+    for nuc in range(len(DNA)):
+        if DNA[nuc] == 'C' or DNA[nuc] == 'G':
+            CG += 1
+    result = CG/len(DNA)
+    return result
 print(DNA1)
-print(DNA2)
-print(DNA3)
-
-
+print(getCG(DNA1))
 
