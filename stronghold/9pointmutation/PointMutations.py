5e2daa707e9fe8af7c139e82f99e1fef0898b087
Jack Cerulo <jcAlpaca@outlook.com>
2023-11-16 19:44:40 -0700

    finished point mutations

---
+++ stronghold/9pointmutation/PointMutations.py
@@ -0,0 +1,10 @@
+with open ('rosalind_hamm.txt', 'r') as file:
+    DNA = file.read().strip().split('\n')
+print(DNA)
+DNA1 = list(DNA[0])
+DNA2 = list(DNA[1])
+mutations = 0
+for nuc in range(len(DNA1)):
+    if DNA1[nuc] != DNA2[nuc]:
+        mutations += 1
+print(mutations)
