a0feff2559667ddf94f8c3fb2c1d19631b22d0c8
Jack Cerulo <jcAlpaca@outlook.com>
2023-11-20 11:28:49 -0700

    finished fibbonacci rabbits

--- stronghold/11mortalrabbits/FibbonacciRabbits.py
+++ stronghold/11mortalrabbits/FibbonacciRabbits.py
@@ -1,18 +1,13 @@
 n = 87
-m = 17
+m = 19
 pop = [1, 1]
-dead = []
-for i in range(0, n):
-    if i < m:
-        dead.append(0)
-    elif i == m:
-        dead.append(-1)
-    else:
-        dead.append(dead[i-1] + dead[i-2] - 1)
+for i in range(2, n):
+    x = pop[i-1] + pop[i-2]
+    if i == m:
+        x-=1
+    elif i > m:
+        x -= pop[i - m - 1]
 
-for i in range(2, n):
-    pop.append(pop[i-1] + pop[i-2])
-for i in range(2, n):
-    pop[i] += dead[i]
+    pop.append(x)
 
 print(pop[n-1])
