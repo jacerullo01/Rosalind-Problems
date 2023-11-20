37a5cdffd2db5d6ccc45e16c6500215d30b25319
Jack Cerulo <jcAlpaca@outlook.com>
2023-11-20 16:48:29 -0700

    working on consensus profile

---
+++ stronghold/cons/ConsensusProfile.py
@@ -0,0 +1,40 @@
+with open ('rosalind_cons.txt', 'r') as file:
+    dataset = file.read().strip().replace('\n', '')
+dataset = dataset.split('>Rosalind_')
+if dataset[0] == '':
+    del dataset[0]
+def count(DNA, nuc):
+    count = 0
+    for nucleotide in DNA:
+        if nucleotide == nuc:
+            count +=1
+    return count
+DNA = []
+profile = []
+for i in range(len(dataset)):
+    DNA.append(list(dataset[i][1:]))
+counter = [[row[i] for row in DNA] for i in range(len(DNA[0]))]
+profile = {'A': [], 'C': [], 'G': [], 'T': []}
+for i in counter:
+    profile['A'].append(count(i, 'A'))
+    profile['C'].append(count(i, 'C'))
+    profile['G'].append(count(i, 'G'))
+    profile['T'].append(count(i, 'T'))
+consensus = ''
+for counts in zip(profile['A'], profile['C'], profile['G'], profile['T']):
+    max_count = max(counts)
+    if max_count == counts[0]:
+        consensus += 'A'
+    elif max_count == counts[1]:
+        consensus += 'C'
+    elif max_count == counts[2]:
+        consensus += 'G'
+    else:
+        consensus += 'T'
+
+
+print(consensus)
+for nucleotide in ['A', 'C', 'G', 'T']:
+    print(f"{nucleotide}: {' '.join(map(str, profile[nucleotide]))}")
+
+
